e0825326595d56af66de02284e1a914e08d28649
Nian Liu <nianliu18@gmail.com>
2018-10-30 16:55:54 -0400

    Revert "addpostbutton"

--- client/src/components/layout/Feed.js
+++ client/src/components/layout/Feed.js
@@ -20,12 +20,6 @@ const mapDispatchToProps = dispatch => ({
   getPost: () => {
     dispatch(actions.getPost())
   },
-
-  addPost: () => {
-    dispatch(actions.addPost())
-  },
-});
-
   upvote: (id, location) => {
     dispatch(actions.upvote(id, location))
   },
@@ -33,6 +27,7 @@ const mapDispatchToProps = dispatch => ({
     dispatch(actions.downvote(id, location))
   }
 });
+
 
 
 
@@ -47,14 +42,14 @@ class Feed extends Component {
 
 
   render() {
-    console.log(this.props.feed, 'this is the feed')
     // console.log(this.props.feed, '----Feed----');
     let allPosts = [];
     let posts = this.props.feed;
     console.log('posts', posts);
 
+
     for (let i = 0; i < posts.length; i++) {
-      var likesCount = 0;
+      let likesCount = 0;
       if (posts[i].likes.length) {
         likesCount = posts[i].likes.length;
       }
@@ -64,20 +59,15 @@ class Feed extends Component {
       // We haven't placed dateReadable in the div yet (still working on layout UX), but it's ready to insert.
       allPosts.push(<Post location={i} id={this.props.feed[i]._id} likesCount={likesCount} text={posts[i].text} tags={posts[i].tags} name={posts[i].name} upvote={this.props.upvote} downvote={this.props.downvote}/>)
     }
-
     // console.log(allPosts, '------all Posts ----')
     return (
       <div>
         <Header user={this.props.auth.user.name}/>
         <div className="feed-container">
-          <Post
-            likesCount = {likesCount}
-            feed = {this.props.feed}
-          />
-          <input placeholder='Write question here'
-            type='text'
-          />
-          <button type="submit" onClick={() => {this.props.addPost()}}> Add Post </button>
+
+          {/* <h3>{this.props.auth.user.name} successfully Logged in!</h3> */}
+          {allPosts}
+          <Logout />
         </div>
       </div>
     )
--- client/src/components/layout/Post.js
+++ client/src/components/layout/Post.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import * as actions from '../../actions/postActions';
+// import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
 
 
 
-
 const Post = (props) => {
-  console.log(props.feed.feed, 'passed down to POST')
-  return (
-    <div>
-      {props.feed.feed.map((post)=> {
-        return (
-          <div className="questionBox" key={post._id}>
-          <ul className='question' key={post._id}>
-          <li> {post.text}</li> </ul>
-          <br></br>
-          {post.tags}
-          <br></br>
-          {post.name}
-          <i className="fas fa-arrow-up"></i>
-          <strong> {props.likesCount} </strong>
-          <i className="fas fa-arrow-down"></i>
-          <hr></hr>
-          </div>
-        );
-      })}
   // console.log(props, 'passed down to POST')
   // let expanded = false;
   // if(expanded) {
@@ -37,21 +16,22 @@ const Post = (props) => {
 
   // } 
 
-//   return (
-//     <div>
-//       <div className="questionBox">
-//       <button onClick={() => props.upvote(`${props.id}`, props.location)}><i className="fas fa-arrow-up" ></i></button>
-//       <strong> {props.likesCount} </strong>
-//       <button onClick={() => props.downvote(`${props.id}`, props.location)}><i className="fas fa-arrow-down" ></i></button>
-//       <Link to={`/posts/${props.id}`}>
-//       <span className='question'>{props.text}<br></br>{props.tags}<br></br>{props.name}<hr></hr></span>
-//       </Link>
-//       </div>
-//     </div>
-//   );
-      
+  return (
+    <div>
+      <div className="questionBox">
+      <button onClick={() => props.upvote(`${props.id}`, props.location)}><i className="fas fa-arrow-up" ></i></button>
+      <strong> {props.likesCount} </strong>
+      <button onClick={() => props.downvote(`${props.id}`, props.location)}><i className="fas fa-arrow-down" ></i></button>
+      <Link to={`/posts/${props.id}`}>
+      <span className='question'>{props.text}<br></br>{props.tags}<br></br>{props.name}<hr></hr></span>
+      </Link>
+      </div>
+    </div>
+  );
 };
 
 
 export default Post;
 
+
+ // allPosts.push(<div key={i} className="questionBox"> <i className="fas fa-arrow-up" key={i}></i> <strong> {likesCount} </strong><i className="fas fa-arrow-down" key={i}></i> <span className='question' key={i}>{posts[i].text}<br></br>{posts[i].tags}<br></br>{posts[i].name}<hr></hr></span></div>)
